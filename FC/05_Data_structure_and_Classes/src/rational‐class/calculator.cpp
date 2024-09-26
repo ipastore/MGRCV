398643385c365d42a2d96b962c31897feeb5526f
padidavid <david.padilla97@gmail.com>
2024-09-26 18:57:41 +0200

    Retoques Assigment_05

--- FC/05_Data_structure_and_Classes/src/rational‐class/calculator.cpp
+++ FC/05_Data_structure_and_Classes/src/rational‐class/calculator.cpp
@@ -2,6 +2,24 @@
 #include "rational.h"
 using namespace std;
 
+/*
+Developed by: 
+    - David Padilla Orenga, NIA: 946874
+    - Inacio Pastore Benaim, NIP: 920576
+
+Description:
+    This program is a simple calculator that works with rational numbers (fractions). 
+    The Rational class provides the functionality to represent, manipulate, and perform
+    arithmetic operations on rational numbers.
+
+    The program continues to ask for input until both rational numbers entered have 0 as
+    numerator, which signals the end of the program.
+
+    The program handles the following features:
+    - Addition, subtraction, multiplication, and division of rational numbers.
+    - Input validation: Denominators cannot be zero, and operations must be valid (+, -, *, /).
+    - The program handles edge cases such as division by zero and unrecognized operators.
+*/
 
 int main() {
  
