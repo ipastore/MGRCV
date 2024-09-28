730de0d66cdb802da7759f0bf69020be3bc21f31
padidavid <david.padilla97@gmail.com>
2024-09-28 17:38:57 +0200

    03_MemoryManagement Finished

---
+++ FC/03_Memory_management/src/max_min_heap_2d_matrix.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <random>
+
+/*
+ * Developed by: 
+ *     - David Padilla Orenga, NIA: 946874
+ *     - Inacio Pastore Benaim, NIP: 920576
+ * 
+ * EXPLANATION:
+ *      - The matrix is dynamically allocated in the heap using several new operators. This means that memory for the matrix rows
+ *        and columns is explicitly allocated at runtime, and we must free this memory when it's no longer needed to avoid memory leaks.
+ * 
+ *      - In this code we have created matrix.array (into the struct). It is a pointer to a pointer (float**), which means it points to an
+ *        array of pointers. This first (float**) points to an array of (float*) pointers representing each of the rows of the matrix 
+ *        (matrix.array = new float*[matrix.rows]). 
+ * 
+ *      - Once created this first array of float pointers, for each row, we allocate memory for an array of float values (one for each column,
+ *        corresponding to the elements of this row).
+ * 
+ *      - Since we're allocating memory dynamically using  several new[], we need to free that memory using delete[] once the program finish.
+ *        Nevertheless, the deleting orders matters in order to avoid memory leaks. First we have to delete each of the float arrays corresponding
+ *        to the elements/columns of each individual row. Secondly, we already can delete de array of pointers corresponding that contains the
+ *        adresses the rows arrays. If this is done the oter way around, after deleting the float** we could not acces the floats* that were inside,
+ *        leading to a memory leak.
+ * 
+ *      - PD: We've decided to create a Matrix struc to concentrate all the matrix information in one data type. We've also created some functions to
+ *        have a more clear main. 
+ *
+ *  */
+
+struct Matrix
+{
+    float** array;
+    size_t rows;
+    size_t columns;
+    float min_value = std::numeric_limits<float>::max();
+    float max_value = std::numeric_limits<float>::lowest(); 
+};
+
+void get_matrix_dimensions(Matrix& matrix){
+    std::cout << "Indicate rows and columns of the matrix:" << std::endl;
+    std::cin >> matrix.rows >> matrix.columns;
+}
+
+void check_dimensions(Matrix& matrix){
+    if ((matrix.rows >= 5) || (matrix.columns >= 5)) {
+        std::cerr << "Error: Rows/columns dimensions cannot be 5 or bigger." << std::endl;
+        exit(1);
+    }
+}
+
+void create_matrix(Matrix& matrix) {
+    // Allocating memory for the rows
+    matrix.array = new float*[matrix.rows];
+    // Allocating memory for each column/element on a row
+    for (size_t i = 0; i < matrix.rows; ++i) {
+        matrix.array[i] = new float[matrix.columns];  
+    }
+}
+
+void fill_matrix(Matrix& matrix, std::uniform_real_distribution<float> dis, std::mt19937 gen){
+
+    std::cout << std::endl << "Used matrix is:" << std::endl << std::endl;
+
+    for(size_t n = 0; n < matrix.rows; n++){
+        for (size_t m = 0; m < matrix.columns; m++){
+            matrix.array[n][m] = dis(gen);  
+            std::cout << matrix.array[n][m] << ' ';
+
+            // Checking min/max
+            if (matrix.array[n][m] < matrix.min_value) matrix.min_value = matrix.array[n][m];
+            if (matrix.array[n][m] > matrix.max_value) matrix.max_value = matrix.array[n][m];
+        }
+        std::cout << std::endl;
+    }
+}
+
+void delete_matrix(Matrix& matrix) {
+    // We have to delete first the columns arrays that are inside the rows array of pointers.
+    // If we would do it the other way, we couldn't access the columsn arrays once deeleted the rows arrays and that would mean memory leaks.
+    for (size_t n = 0; n < matrix.rows; ++n) {
+        delete[] matrix.array[n];
+    }
+    delete[] matrix.array;
+}
+
+int main() {
+
+    std::random_device rd; 
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis {0.0f, 1.0f};
+    Matrix M;
+
+    get_matrix_dimensions(M);
+    check_dimensions(M);
+    create_matrix(M);
+    fill_matrix(M, dis, gen);
+
+    std::cout << std::endl << "The maximum and minimun values of the matrix are: " << M.max_value << " and " << M.min_value << std::endl;
+
+    delete_matrix(M);
+}
--- FC/03_Memory_management/src/to_fix_missing_new.cpp
+++ FC/03_Memory_management/src/to_fix_missing_new.cpp
@@ -2,23 +2,28 @@
 using namespace std;
 
 /*
-Developed by: 
-    - David Padilla Orenga, NIA: 946874
-    - Inacio Pastore Benaim, NIP: 920576
+ * Developed by: 
+ *     - David Padilla Orenga, NIA: 946874
+ *     - Inacio Pastore Benaim, NIP: 920576
+ * 
+ * ERROR WITHOUT THE FIX:
+ *     array[0]: 6
+ *     [1]    73522 segmentation fault 
+ * 
+ * SOLUTION:
+ *     - Segmentation fault means that the code is trying to access a forbidden memory address. To fix this we implemented
+ *     a new pointer with a valid address. Afterwards, to demonstrate the functionality of reassigning a pointer, first we delete
+ *     former pointer, then reassigned it to the valid address.
+ * 
+ *     SOLUTION:
+ *     - Segmentation fault means that the code is trying to access a forbidden memory address. We have comment this line to avoid that.
+ *     In general, pointing to a random memory direction should be avoided because we would have an undefined behaviour.
+ * 
+ *     - We also have added to the delete_two_array funtion '[]' to the delete in order to free the entire array memory. Using only 'delete'
+ *     is undefined behaviour and it may appear to work but there is no guarantee.
+ */
 
-ERROR WITHOUT THE FIX:
-    array[0]: 6
-    [1]    73522 segmentation fault 
 
-SOLUTION:
-    - Segmentation fault means that the code is trying to access a forbidden memory address. We have comment this line to avoid that.
-    In general, pointing to a random memory direction should be avoided.
-
-    - We also have added to the delete_two_array funtion '[]' to the delete in order to delete the entire array. Using only 'delete'
-    is undefined behaviour and it may appear to work but there is no guarantee. Take into account that the last
-    cout printing "array[1]" was also commented out because the delete[] was performed before and there is no reason
-    to access it again.
-*/
 
 int* create_two_array(){
     int* ptr = new int[2];
@@ -31,11 +36,21 @@ void delete_two_array(int* ptr){
 
 int main(){
     int* array=create_two_array();
+    int* valid_address = create_two_array();
+
     array[0] = 6;
     array[1] = 7;
-    cout << "array[0]: " << array[0] << endl;
-    cout << "array[1]: " << array[1] << endl;
+    valid_address[0] = 34;
+    valid_address[1] = 12;
+    cout << "array[0]: " << array[0] << endl; // It prints 6
+    cout << "array[1]: " << array[1] << endl; // It prints 7
     delete_two_array(array);
-    // array=reinterpret_cast<int*>(0xDEADBEEF); // Is 0xDEADBEEF a valid address?
-    // cout << "array[1]: " << array[1] << endl;
+    // array=reinterpret_cast<int*>(0xDEADBEEF); // Not recommended to try to acces to a random memory adress. Undefined behaviour.
+    array = valid_address; // We've played in oreder to reassing the 'array' pointer to an existing one.
+    cout << "array[0]: " << array[0] << endl; // It prints 34 since is now poiting to the same adress as 'valid_address'.
+    cout << "array[1]: " << array[1] << endl; // It prints 12 since is now poiting to the same adress as 'valid_address'.
+    delete_two_array(valid_address); // This line deallocate the memory where  'array' and 'valid_address' are pointing. Next two lines would have an undifined behaviour.
+    cout << "array[0]: " << array[0] << endl; // Random number --> UNDEFINED BEHAVIOUR 
+    cout << "array[1]: " << array[1] << endl; // Random number --> UNDEFINED BEHAVIOUR 
 }
+
