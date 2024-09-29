542241036c0807720f34fff343a5a40ea176b8ee
padidavid <david.padilla97@gmail.com>
2024-09-29 18:09:29 +0200

    Advances on smart pointers

---
+++ FC/04_Smart_pointers/src/shared_ptr.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <memory>
+
+/*
+ * Developed by: 
+ *     - David Padilla Orenga, NIA: 946874
+ *     - Inacio Pastore Benaim, NIP: 920576
+ * 
+ * DESCRIPTION:
+ * 
+ * 
+ * QUESTIONS: 
+ * 1. How many objects (variables) are allocated in the stack and in the heap?
+ *  STACK OBJECTS:
+ *  - There are two objects allocated in the STACK: sp1 and sp2.
+ *  - The sp1 object is created in the main scope, so it is accessible for all the main domain. 
+ *  - The sp2 object is created in the new scope indicated by the inner {}. It is only reachable in this {} domain.
+ *  - Both objects are shared pointers, meaning that they are automatically destroyed when they go out of range.
+ *  - Sp1 is destroyed when main function ends. Sp2 object is destroyed when {} domain ends.
+ *  
+ *  HEAP OBJECTS:
+ *  - There is one object allocated on the heap: std::make_shared<int>(1).
+ *  - This is a integer pointer with a value of 1 allocated on the heap and managed by sp1 and sp2.
+ * 
+ * 2. Can both sp1 and sp2 modify the object in the heap?
+ *  - Yes, they can. We are using smart pointer that are used to control the ownership of pointers in order to 
+ *    read and write on the heap. In this case, we are using shared_pointer which allow multiple pointers to share
+ *    ownership of the same resource. This means that both can read from and write to the same object in the heap.
+ * 
+ * 3. Does the destruction of sp2 releases the memory allocated in the heap?
+ *  - No, it doesn't. The shared_pointers for a same address don't realease the memory on the heap until all the shared
+ *    pointers pointing to this address are destroyed.
+ *  - The output of the code give us:
+ *      sp1.use_count(): 1
+ *      sp1.use_count(): 2
+ *      sp2.use_count(): 2
+ *      sp1.use_count(): 1
+ *      Is sp1 unique? 1
+ *  - As seen in the output, when sp2 goes out of scope, this smart_pointer or reference is destroyed but sp1 refernce is
+ *    still remaining unitl main functions ends. Therefore, sp2 destruction doesn't mean the realeases of the memory allocated
+ *    on the heap.    
+ * 
+ */
+
+int main() {
+  auto sp1 = std::make_shared<int>(1);
+  std::cout << "sp1.use_count(): " << sp1.use_count() << std::endl;
+  { // new scope
+    std::shared_ptr<int> sp2(sp1);
+    std::cout << "sp1.use_count(): " << sp1.use_count() << std::endl;
+    std::cout << "sp2.use_count(): " << sp2.use_count() << std::endl;
+  }
+  std::cout << "sp1.use_count(): " << sp1.use_count() << std::endl;
+  std::cout << "Is sp1 unique? " << sp1.unique() << std::endl;
+}
--- FC/04_Smart_pointers/src/to_fix_memory_leaks.cpp
+++ FC/04_Smart_pointers/src/to_fix_memory_leaks.cpp
@@ -19,65 +19,36 @@ struct point_node
 };
 
 
-point* copy_point(const point& p)
+
+std::unique_ptr<point_node> copy_point(const point& p)
 {
-    point* point_ptr = new point(p);
+    auto point_ptr = std::make_unique<point_node>(p);
     if (p.x > 1)
         return nullptr;
     return point_ptr;
 }
 
 
-int main()
-{
+int main(){
     const size_t n = 4;
     point points[n]={ {0, 0}, {1, 1}, {0, 0}, {1, 1} };
 
     // save points whose coordinates are not (0, 0)
     std::unique_ptr<point_node> list = nullptr;
 
-    for(const auto& p: points) {
-        std::unique_ptr<point_node> node = std::make_unique<point_node>(p);
-        
-        if(p.not_zero()) {
-            if(list == nullptr) {
-                list = node;
-            } else {
-                point_node* iter=list;
-                while(iter->next != nullptr) {
-                    iter = iter->next;
+    for (const auto& p : points) {
+        // point_node* node = new point_node(p, nullptr);
+        std::unique_ptr<point_node> node  = std::make_unique<point_node>(p);
+
+        if (p.not_zero()) {
+            if (list == nullptr) {
+                list = std::move(node);;
+            } else { 
+                point_node* current = list.get(); 
+                while (current->next != nullptr) {
+                    current = current->next.get();
                 }
-                iter->next = node;
-            }
-        }
-    }
-    
-    // print non-zero nodes
-    for(point_node* iter = list; iter != nullptr; iter = iter->next) {
-        std::cout << "(x, y): " << iter->p.x << ", " << iter->p.y << std::endl;
-    }
-}
-
-int main()
-{
-    const size_t n = 4;
-    point points[n]={ {0, 0}, {1, 1}, {0, 0}, {1, 1} };
-
-    // save points whose coordinates are not (0, 0)
-    std::unique_ptr<point_node> list = nullptr;
-
-    for(const auto& p: points) {
-        auto node = std::make_unique<point_node>(p);  // Create a new point_node using std::unique_ptr
-        
-        if(p.not_zero()) {
-            if(list == nullptr) {
-                list = std::move(node);  // If list is empty, point it to the new node
-            } else {
-                point_node* iter = list.get();  // Get a raw pointer to traverse the list
-                while(iter->next != nullptr) {
-                    iter = iter->next.get();  // Use get() to access the raw pointer
-                }
-                iter->next = std::move(node);  // Append the new node at the end
+                current->next = std::move(node);
             }
         }
     }
