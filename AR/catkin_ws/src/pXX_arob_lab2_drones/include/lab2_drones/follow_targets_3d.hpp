223286aa6951e61bd35be30b739243841ab07f4b
padidavid <david.padilla97@gmail.com>
2024-10-17 16:42:33 +0200

    Lab 2 finished

--- AR/catkin_ws/src/pXX_arob_lab2_drones/include/lab2_drones/follow_targets_3d.hpp
+++ AR/catkin_ws/src/pXX_arob_lab2_drones/include/lab2_drones/follow_targets_3d.hpp
@@ -14,30 +14,37 @@
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <nav_msgs/Odometry.h>
-#include <tf/transform_broadcaster.h> // The added dependency should be tf2
+#include <tf2_ros/transform_broadcaster.h> // The added dependency should be tf2 ----> DONE
 
 class FollowTargets3D {
 
 public:
 	FollowTargets3D(ros::NodeHandle nh );
-
     ~FollowTargets3D() {}
 
 private:
     // INFO: It is a convention to use _ for private variables and methods
+    // VARIABLES
     ros::NodeHandle nh_;
 
-    // Declare your publisher and subscribers
+    // Publisher and subscribers
+    ros::Publisher goal_pub_;
+    ros::Subscriber odometry_sub_;
 
+    // Timer for continuous goal publishing
+    ros::Timer goal_timer_;
+
+    bool odometry_received_;
     std::string targets_file_path_;
     std::vector<geometry_msgs::Pose> targets_; //List of goal targets
-    int current_goal_index_;
     geometry_msgs::PoseStamped current_goal_;
+    geometry_msgs::Point current_position_;
+    int current_goal_index_;
 
+    // METHODS
     bool readTargets_(std::string file);
-    
     //  EXERCISE: Implement the logic to publish new goals when the previous one is reached
-    //  (more info in the constructor of this class).
+    void goalTimerCallback_(const ros::TimerEvent& event);
     void odometryCallback_(const nav_msgs::Odometry& msg);
 };
 
--- AR/catkin_ws/src/pXX_arob_lab2_drones/src/follow_targets_3d.cpp
+++ AR/catkin_ws/src/pXX_arob_lab2_drones/src/follow_targets_3d.cpp
@@ -1,4 +1,5 @@
 #include "lab2_drones/follow_targets_3d.hpp"
+#include <ros/package.h>
 
 FollowTargets3D::FollowTargets3D(ros::NodeHandle nh) : nh_(nh)
 {
@@ -7,7 +8,7 @@ FollowTargets3D::FollowTargets3D(ros::NodeHandle nh) : nh_(nh)
     if (!nh_.getParam("targets_file_path", targets_file_path_))
     {
         ROS_WARN("There is no 'targets_file_path' parameter. Using default value.");
-        targets_file_path_ = "/home/arob/catkin_ws/src/pXX_arob_lab2_drones/data/targets.txt";
+        targets_file_path_ = "/home/padidavid/Documents/unizar/master/repositorios/MGRCV/AR/catkin_ws/src/pXX_arob_lab2_drones/data/targets.txt";
     }
     // Try to open the targets file.
     if (!readTargets_(targets_file_path_))
@@ -21,28 +22,35 @@ FollowTargets3D::FollowTargets3D(ros::NodeHandle nh) : nh_(nh)
     current_goal_.pose.position = targets_[current_goal_index_].position;
     // Even if we only want the position, it is a good idea to initialize rotation to zero.
     current_goal_.pose.orientation = geometry_msgs::Quaternion();
-    // ADDITIONAL EXERCISE: : Usually, it is a good practice to wait until odometry is received before sending
+    
+    
+    // ADDITIONAL EXERCISE: Usually, it is a good practice to wait until odometry is received before sending
     // any goal. Implement the logic to achieve this.
+    odometry_received_ = false; // Flag for domometry reception
+    // Subscriber to odometry
+    odometry_sub_ = nh_.subscribe("/ground_truth/state", 10, &FollowTargets3D::odometryCallback_, this);
 
     // EXERCISE: Define your publishers and subscribers
     // Find the topic to send pose commands and the type of message it receives
-
-    // EXERCISE: Find a topic to listen to the position of the quadrotor in order to send a new goal
-    // once the previous is reached. 
-    // INFO: In order to pass as a callback a member of a function, we have to pass a reference 
-    // to the callback method and a reference to the object of the class:
-    // https://wiki.ros.org/roscpp_tutorials/Tutorials/UsingClassMethodsAsCallbacks
-    // (THE LAST SENTENCE IN SUBSCRIPTIONS IS REALLY IMPORTANT)
+    // Publisher for the goal
+    goal_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/command/pose", 10);
 
     // ADDITIONAL EXERCISE: In some cases, robot controllers might need to receive the goal continuously
     // (e.g. at a frequency of 100Hz).
     // You will have to save the next_goal_id and send the goal in an interval.
     // Find how this is commonly done in ROS.
+    // Timer to periodically send the goal if odometry is received
+    goal_timer_ = nh_.createTimer(ros::Duration(0.01), &FollowTargets3D::goalTimerCallback_, this);
+   
+    // INFO: In order to pass as a callback a member of a function, we have to pass a reference 
+    // to the callback method and a reference to the object of the class:
+    // https://wiki.ros.org/roscpp_tutorials/Tutorials/UsingClassMethodsAsCallbacks
+    // (THE LAST SENTENCE IN SUBSCRIPTIONS IS REALLY IMPORTANT)
 
     ROS_INFO("FollowTargets3D initialized");
 }
 
-bool FollowTargets3DSol::readTargets_(std::string file_path)
+bool FollowTargets3D::readTargets_(std::string file_path)
 {
     //Open the file
     std::ifstream input_file;
@@ -86,5 +94,66 @@ bool FollowTargets3DSol::readTargets_(std::string file_path)
 
 void FollowTargets3D::odometryCallback_(const nav_msgs::Odometry& msg)
 {
+    if (!odometry_received_) {
+        // Odometry flag is set to FALSE by default
+        // When the odometry is received for the first time, the flag is set to TRUE
+        // Once it's TRUE will never get here again and we'ĺl start calculating the distance to the goal
+        odometry_received_ = true;
+        ROS_INFO("Odometry received. Ready to start sending goals.");
+    } else {
+        // EXERCISE: Find a topic to listen to the position of the quadrotor in order to send a new goal
+        // once the previous is reached.
+        // We get the current position of the drone
+        current_position_ = msg.pose.pose.position;
 
+        // We define a distance thresholde to know if we have reach (aprox) the goal
+        const double goal_threshold = 0.5; 
+
+        // We calculate the euclidean distance to the goal to check if we have reached it
+        double distance_to_goal = std::sqrt(
+            std::pow(current_goal_.pose.position.x - current_position_.x, 2) +
+            std::pow(current_goal_.pose.position.y - current_position_.y, 2) +
+            std::pow(current_goal_.pose.position.z - current_position_.z, 2)
+        );
+
+        // WE check if goal is reached
+        if (distance_to_goal < goal_threshold) {
+            // If we have more goals, we set the next goal in case there is one
+            if (current_goal_index_ < targets_.size() - 1) {
+            current_goal_index_++;
+            current_goal_.pose.position = targets_[current_goal_index_].position;
+            current_goal_.pose.orientation = geometry_msgs::Quaternion(); // Reset orientation
+            ROS_INFO("Reached goal. Sending next goal [%.2f, %.2f, %.2f]",
+                     current_goal_.pose.position.x, 
+                     current_goal_.pose.position.y, 
+                     current_goal_.pose.position.z);
+            } else {
+                // If we have reached all goals, we stop the drone
+                ROS_INFO("All goals reached!");
+            }
+        }
+        
+    }
+
+}
+
+void FollowTargets3D::goalTimerCallback_(const ros::TimerEvent& event)
+{
+    // If odometry is not received, we do not send goals
+    if (!odometry_received_) {
+        odometry_received_ = true;
+        ROS_INFO("Waiting for odometry data before sending goals.");
+        return;
+    }
+
+    // Publish the goal
+    goal_pub_.publish(current_goal_);
+    ROS_INFO_THROTTLE(1, "Current position: [%.2f, %.2f, %.2f]", 
+                      current_position_.x, 
+                      current_position_.y, 
+                      current_position_.z);
+    ROS_INFO_THROTTLE(1, "Publishing goal: [%.2f, %.2f, %.2f]", 
+                      current_goal_.pose.position.x, 
+                      current_goal_.pose.position.y, 
+                      current_goal_.pose.position.z);
 }
