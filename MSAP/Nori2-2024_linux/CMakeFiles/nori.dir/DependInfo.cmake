03d9594726e353ceab6fd874ab57f2ea5fccee6e
padidavid <david.padilla97@gmail.com>
2024-10-21 18:33:34 +0200

    Lab 1 finished

---
+++ MSAP/Nori2-2024_linux/CMakeFiles/nori.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/padidavid/Downloads/Nori2-2024/src/accel.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/accel.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/area.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/area.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/bitmap.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/bitmap.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/block.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/block.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/chi2test.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/chi2test.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/common.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/common.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/dielectric.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/dielectric.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/diffuse.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/diffuse.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/direct_whitted.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/direct_whitted.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/environment.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/environment.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/gui.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/gui.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/independent.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/independent.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/main.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/main.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/mesh.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/mesh.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/microfacet.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/microfacet.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/mirror.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/mirror.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/normals.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/normals.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/obj.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/obj.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/object.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/object.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/parser.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/parser.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/perspective.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/perspective.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/pointlight.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/pointlight.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/proplist.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/proplist.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/rfilter.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/rfilter.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/scene.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/scene.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/texture.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/texture.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/ttest.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/ttest.cpp.o"
+  "/home/padidavid/Downloads/Nori2-2024/src/warp.cpp" "/home/padidavid/Downloads/Nori2-2024/CMakeFiles/nori.dir/src/warp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "ext/tinyformat"
+  "ext/nanogui/ext/eigen"
+  "ext/openexr/IlmBase/Imath"
+  "ext/openexr/IlmBase/Iex"
+  "ext/openexr/IlmBase/Half"
+  "ext/openexr/OpenEXR/IlmImf"
+  "ext_build/openexr/OpenEXR/config"
+  "ext_build/openexr/IlmBase/config"
+  "ext/tbb/include"
+  "ext/pcg32"
+  "ext/pugixml/src"
+  "ext/hypothesis"
+  "ext/nanogui/ext/glfw/include"
+  "ext/nanogui/ext/glew/include"
+  "ext/nanogui/ext/nanovg/src"
+  "ext/nanogui/include"
+  "ext/filesystem"
+  "ext/nanogui/ext/nanovg/example"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/padidavid/Downloads/Nori2-2024/ext_build/tbb/CMakeFiles/tbb_static.dir/DependInfo.cmake"
+  "/home/padidavid/Downloads/Nori2-2024/ext_build/CMakeFiles/pugixml.dir/DependInfo.cmake"
+  "/home/padidavid/Downloads/Nori2-2024/ext_build/openexr/OpenEXR/IlmImf/CMakeFiles/IlmImf.dir/DependInfo.cmake"
+  "/home/padidavid/Downloads/Nori2-2024/ext_build/nanogui/CMakeFiles/nanogui.dir/DependInfo.cmake"
+  "/home/padidavid/Downloads/Nori2-2024/ext_build/openexr/IlmBase/Half/CMakeFiles/Half.dir/DependInfo.cmake"
+  "/home/padidavid/Downloads/Nori2-2024/ext_build/openexr/IlmBase/Imath/CMakeFiles/Imath.dir/DependInfo.cmake"
+  "/home/padidavid/Downloads/Nori2-2024/ext_build/openexr/IlmBase/IlmThread/CMakeFiles/IlmThread.dir/DependInfo.cmake"
+  "/home/padidavid/Downloads/Nori2-2024/ext_build/openexr/IlmBase/Iex/CMakeFiles/Iex.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
