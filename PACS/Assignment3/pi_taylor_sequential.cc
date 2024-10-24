99cce5e6f00a199e524ed9eecf4553ab5d5fe3c7
padidavid <david.padilla97@gmail.com>
2024-10-24 15:57:56 +0200

    Pacs 3 started

--- PACS/Assignment3/pi_taylor_sequential.cc
+++ PACS/Assignment3/pi_taylor_sequential.cc
@@ -1,29 +1,52 @@
+// pi_taylor_sequential.cc
+
 #include <iomanip>
 #include <iostream>
-#include <limits>
+#include <climits>
+#include <chrono>
+
 
 // Allow to change the floating point type
 using my_float = long double;
 
 my_float pi_taylor(size_t steps) {
 
-    (void)steps; // ensure no warnings with unused variable
-    return 0.0f;
+    my_float pi_aprox = 0;
+    size_t n = 0;
+    int sign =1;
+		// No se si es igual o igual o menos. PREGUNTAR
+    
+    while(n <= steps){
+	    pi_aprox += sign / (2.0 * n + 1.0);
+	    sign = -sign;
+	    n++;
+    }
+
+    
+
+    return 4.0 * pi_aprox;
 }
 
 int main(int argc, const char *argv[]) {
-
-    // read the number of steps from the command line
     if (argc != 2) {
         std::cerr << "Invalid syntax: pi_taylor <steps>" << std::endl;
-        exit(1);
-
+        return 1;
     }
 
     size_t steps = std::stoll(argv[1]);
+
+    // Start time measurement
+    auto start = std::chrono::high_resolution_clock::now();
+
     auto pi = pi_taylor(steps);
 
-    std::cout << "For " << steps << ", pi value: "
-        << std::setprecision(std::numeric_limits<my_float>::digits10 + 1)
-        << pi << std::endl;
+    // End time measurement
+    auto end = std::chrono::high_resolution_clock::now();
+
+    // Calculate the duration
+    std::chrono::duration<double> duration = end - start;
+
+    // Print the results
+    std::cout << "For " << steps << " steps, pi value: " << pi << std::endl;
+    std::cout << "Execution time: " << duration.count() << " seconds" << std::endl;
 }
